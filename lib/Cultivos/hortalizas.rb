515c349f684a39f5314d767958596436a2ee8366
Marta Gonzalez <alu0101254487@ull.edu.es>
2021-01-24 17:19:13 +0000

    Entregable1 rotacion a+b del miercoles

--- lib/Cultivos/hortalizas.rb
+++ lib/Cultivos/hortalizas.rb
@@ -2,4 +2,26 @@ require "./lib/Cultivos/cultivo"
 
 class Hortalizas < Cultivo
 
+  include Comparable
+
+  attr_reader :comestible, :siembra
+
+  def initialize(name, co2, m2, pvp, coste, comestible, siembra, ciclo)
+    super(name, co2, m2, pvp, coste)
+    @comestible, @siembra, @ciclo = comestible, siembra, ciclo
+  end
+
+  def <=> (other)
+    return nil unless other.instance_of? Hortalizas
+    @pvp <=> other.pvp
+  end
+
+  def numPlants (metros2)
+    return (metros2/m2).to_i
+  end
+
+  def fechaCosecha (time)
+    return time + (@ciclo * 86400)
+  end
+
 end
--- spec/hortalizas_spec.rb
+++ spec/hortalizas_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe Cultivos do
     describe Hortalizas do
 
         before :each do
-            @lechuga = Hortalizas.new("Lechuga", 0.5, 30.0, 1.2, 0.1)
+            @lechuga = Hortalizas.new("Lechuga", 0.5, 0.04, 1.2, 0.1, "Hoja", "Indirecta", 60)
         end
 
         it "Se debe poder instanciar un objeto de la clase Hortalizas" do
@@ -20,7 +20,7 @@ RSpec.describe Cultivos do
 
 
         it "Existe un método para obtener el terreno utilizado" do
-            expect(@lechuga.m2).to eq(30.0)
+            expect(@lechuga.m2).to eq(0.04)
         end
 
         it "Existe un método para obtener el precio de venta" do
@@ -32,7 +32,47 @@ RSpec.describe Cultivos do
         end
 
         it "Existe un metodo para obtener una cadena con la información de la Hortaliza formateada" do
-            expect(@lechuga.to_s).to eq("Nombre: Lechuga\nEmisiones de gases de efecto invernadero: 0.5\nTerreno utilizado: 30.0\nPrecio de venta: 1.2\nCoste de producción: 0.1")
+            expect(@lechuga.to_s).to eq("Nombre: Lechuga\nEmisiones de gases de efecto invernadero: 0.5\nTerreno utilizado: 0.04\nPrecio de venta: 1.2\nCoste de producción: 0.1")
+        end
+
+        it "Se espera que una instancia de la clase Hortaliza sea una Hortaliza" do
+            expect(@lechuga.instance_of? Hortalizas).to eq(true) 
+        end
+
+        it "Se espera que una instancia de la clase Hortaliza sea un Cultivo" do
+            expect(@lechuga.is_a? Cultivo).to eq(true) 
+        end
+
+        it "Se espera que una instancia de la clase Hortaliza sea un Comparable" do
+            expect(@lechuga.is_a? Comparable).to eq(true) 
+        end
+
+        it "No se espera que una instancia de la clase Hortaliza sea una Cadena" do
+            expect(@lechuga.is_a? String).to eq(false) 
+        end
+
+        it "No se espera que una instancia de la clase Hortaliza sea un Número" do
+            expect(@lechuga.is_a? Numeric).to eq(false) 
+        end
+
+        it "Las hortalizas han de poder clasificarse según su parte comestible" do
+            expect(@lechuga.comestible).to eq("Hoja") 
+        end
+
+        it "Las hortalizas han de poder clasificarse según su siembra" do
+            expect(@lechuga.siembra).to eq("Indirecta") 
+        end
+
+        it "Las hortalizas han de ser comparables según el precio de venta" do
+            expect(@lechuga == @lechuga).to eq(true)
+        end
+
+        it "Dada una cantidad de metros cuadrados se ha de estimar el n´umero de plantas que se pueden sembrar de una hortaliza" do
+            expect(@lechuga.numPlants(2)).to eq(50)
+        end
+
+        it "Dada una fecha de siembra se ha de estimar la fecha de la cosecha de una hortaliza" do
+            expect(@lechuga.fechaCosecha(Time.new(2021, 01, 24))).to eq(Time.new(2021, 03, 25))
         end
 
     end
