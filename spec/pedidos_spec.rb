2dd9a1099183d924691bac0032e570a9ec0e9ded
Marta Gonzalez <alu0101254487@ull.edu.es>
2021-01-25 15:09:54 +0000

    Añadido dos expectativas en el pedidos_spec.rb

--- spec/pedidos_spec.rb
+++ spec/pedidos_spec.rb
@@ -14,13 +14,22 @@ RSpec.describe Cultivos do
                 specification("El pedido de berro", :time => "es puntual")
 
             end
+
         end
 
-        it "Se crea" do
+        it "Se crea un objeto de tipo Pedido" do
             expect(@pedido).not_to eq(nil)
         end
 
-        it "Se imprime bien el ticket" do
+        it "Escriba el método que permite representar el pedido de una cantidad determinada del producto hortaliza" do
+            expect(@pedido.product(HortalizasEco.new("Acelga", 0.5, 30.0, 2.25, 2.00, 0, "Elevada"), :weight => "100")).to eq("\t[100 kilos] (Acelga - 2.25 - 2.0 - Elevada")
+        end
+
+        it "Escriba el método que permite representar la especificación de la permanencia de la solicitud de una hortaliza" do
+            expect(@pedido.specification("Tiempo del contrato de la acelga", :time => "un año")).to eq("Tiempo del contrato de la acelga un año")
+        end
+
+        it "Se imprime bien el ticket del pedido" do
             expect(@pedido.to_s).to eq("Mercado de La Laguna\\n====================\n\nProductos:\n\t[100 kilos] (Acelga - 2.25 - 2.0 - Elevada\n\t[50 kilos] (Apio - 3.1 - 2.85 - Media\n\t[20 kilos] (Berro - 2.5 - 2.25 - Baja\nEspecificaciones:\n1) Tiempo del contrato de la acelga un año\n2) Tiempo del contrato del apio un mes\n3) El pedido de berro es puntual\n")
         end
 
